6aefd41019d520c5d266ee071d84516b04147126
lildev1-admin <skap0329@naver.com>
2023-04-26 17:14:11 +0900

    init: Gongbok Project Start

---
+++ src/main/java/Gongbok_BE/Gongbok/GongbokApplication.java
@@ -0,0 +1,13 @@
+package Gongbok_BE.Gongbok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongbokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongbokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Gongbok_BE/Gongbok/GongbokApplicationTests.java
@@ -0,0 +1,13 @@
+package Gongbok_BE.Gongbok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongbokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
